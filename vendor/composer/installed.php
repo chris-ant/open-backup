1a4e9b32f803ff42d1dc4aa313a9c30eb15710b6
Chris Ant <chrisant@posteo.net>
2024-07-23 21:54:39 +0300

    add composer autoload files to plugin

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'chrisant/open-backup',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4163e79094c66909818f90ca0514f74fe89b23a9',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'chrisant/open-backup' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4163e79094c66909818f90ca0514f74fe89b23a9',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
